16bf232b49291897ea8dc404383379a1b471f7c4
USER <USER@DESKTOP-HHBG4TS>
2021-04-19 16:16:29 +0900

    commit

---
+++ src/main/java/com/disposable/demo/DisposableProject1Application.java
@@ -0,0 +1,13 @@
+package com.disposable.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisposableProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisposableProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/disposable/demo/DisposableProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disposable.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisposableProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
